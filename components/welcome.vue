98b856add23bbf7466f3e947bc6fc2e03580ebfe
Jeppe Steen <jeppe-steen@live.dk>
2025-11-13 16:41:34 +0100

    welcomepage

--- components/welcome.vue
+++ components/welcome.vue
@@ -1,7 +1,8 @@
 <template>
     <section class="welcome__main">
-        <h2>Velkommen til min akupunkturklinik, Klinik Egernbo.</h2>
-        <p>Har du gået med tanken om at prøve akupunktur, enten af ren nygerighed, eller i håbet om en alternativ vej til at få det bedre fysisk eller mentalt? Så læs endeligt videre, du er kommet til det rette sted!</p>
+        <h2>Velkommen til min akupunkturklinik</h2>
+        <p>Har du gået med tanken om at prøve akupunktur, enten af ren nygerighed, eller i håbet om en alternativ vej til at få det bedre fysisk eller mentalt?</p>
+        <p>Så læs endeligt videre, du er kommet til det rette sted!</p>
     </section>
 </template>
 
@@ -14,5 +15,10 @@
     width: 100%;
     height: 30vh;
     background-color: #ECDEBF;
+    text-align: center;
+}
+h2 {
+    margin-bottom: 10px;
+    text-transform: uppercase;
 }
 </style>
