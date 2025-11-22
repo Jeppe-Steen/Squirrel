321600e71d6e217415421fc2e4c56c34aa213e8f
Jeppe Steen <jeppe-steen@live.dk>
2025-11-22 22:17:13 +0100

    changes to welcome.vue

---
+++ components/welcome.vue
@@ -0,0 +1,27 @@
+<script setup>
+import generalText from "../content/json/generalText.json";
+const text = generalText.data.welcome;
+</script>
+
+<template>
+    <section class="welcome__main">
+        <h2>{{ text.header }}</h2>
+        <p>{{ text.text }}</p>
+    </section>
+</template>
+
+<style lang="scss">
+    .welcome__main {
+        width: 100%;
+        height: fit-content;
+        padding: 50px 0;
+
+        h2 {
+            font-size: 2rem;
+            margin-bottom: 10px;
+        }
+        p {
+            font-size: 1rem;
+        }
+    }
+</style>
