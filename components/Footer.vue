0e4803ecab213f0263e815331381ffd1b3c7040e
Jeppe Steen <61780753+Jeppe-Steen@users.noreply.github.com>
2026-02-03 21:54:52 +0100

    additional things

--- components/Footer.vue
+++ components/Footer.vue
@@ -4,15 +4,13 @@
       <article>
         <h3>Links</h3>
         <ul>
-          <li><a href="#behandlinger">Behandlinger jeg tilbyder</a></li>
-          <li><a href="#akupunktur">Hvad er akupunktur</a></li>
-          <li><a href="#priser">Priser</a></li>
-          <li><a href="#om">Om mig</a></li>
-          <li><a href="#kontakt">Kontakt</a></li>
-          <li><a href="#faq">Ofte stillede spørgsmål</a></li>
-          <li><a href="#persondatapolitik">Persondatapolitik</a></li>
-          <li><a href="#samtykke">Samtykkeerklæring</a></li>
-          <li><a href="#handelsbetingelser">Handelsbetingelser</a></li>
+          <li><NuxtLink to="/">Forside</NuxtLink></li>
+          <li><NuxtLink to="/behandlinger">Behandlinger jeg tilbyder</NuxtLink></li>
+          <li><NuxtLink to="/priser">Priser</NuxtLink></li>
+          <li><NuxtLink to="/kontakt">Kontakt</NuxtLink></li>
+          <li><NuxtLink to="/betingelser">Betingelser</NuxtLink></li>
+          <li><NuxtLink to="/om">Om klinikken</NuxtLink></li>
+          <li><NuxtLink to="/#faq">Ofte stillede spørgsmål</NuxtLink></li>
         </ul>
       </article>
       <address>
@@ -22,8 +20,11 @@
         <p>Mail: info@klinikegernboakupunktur.dk</p>
         <p>CVR: 46094417</p>
         <br />
-        <p>Mandag-fredag: 9:00 - 15:00</p>
-        <p>Lørdag-Søndag: Lukket</p>
+        <p>Mandag: 8:00 - 15:00</p>
+        <p>Tirsdag: 8:00 - 18:30</p>
+        <p>Onsdag - Torsdag: 8:00 - 15:00</p>
+        <p>Fredag: 8:00 - 12:30</p>
+        <p>Lørdag - Søndag: Lukket</p>
       </address>
       <article>
         <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2163.6064533883787!2d9.696345913228498!3d57.160883503077045!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x464925c8a65bab49%3A0xb1b741175a25d19e!2sSandmosevej%2011%2C%209440%20Aabybro!5e0!3m2!1sda!2sdk!4v1768941476115!5m2!1sda!2sdk" width="600" height="450" style="border:0;" allowfullscreen="false" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
