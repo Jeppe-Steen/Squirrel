9c893067b525e1f4da635758760162ef03d8287c
Jeppe Steen <61780753+Jeppe-Steen@users.noreply.github.com>
2026-02-03 20:06:02 +0100

    adding aditional things

--- components/about.vue
+++ components/about.vue
@@ -14,7 +14,7 @@ const array = [
     <section class="about">
         <div class="me">
             <ancor path="om" />
-            <img src="../assets/image/treatments/me_1.webp" alt="me"/>
+            <img src="../assets/image/treatments/me_1.webp" alt="me" loading="lazy" decoding="async"/>
         </div>
         <div class="text">
             <h2>Lidt om mig!</h2>
---
+++ components/reuseable/Treatment.vue
@@ -0,0 +1,38 @@
+<script setup>
+const props = defineProps(['title', 'text']);
+
+</script>
+
+<template>
+    <section class="treatment__main">
+        <h2>{{ props.title}}</h2>
+        <div v-for="item in props.text">
+            <p v-if="!Array.isArray(item)">{{ item }}</p>
+            <ul v-else>
+                <li v-for="subItem in item">{{ subItem }}</li>
+            </ul>
+        </div>
+    </section>
+</template>
+
+<style lang="scss">
+    .treatment__main {
+        width: 100%;
+        height: fit-content;
+        text-align: left;
+        margin: 50px 0;
+
+        h2 {
+            margin-bottom: 20px;
+        }
+
+         p {
+            margin-bottom: 10px;
+        }
+
+        ul {
+            padding-left: 20px; 
+            margin-bottom: 20px;
+        }
+    }
+</style>
