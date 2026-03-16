9e32c8f5110d5595650f2eea03240e68d016efcb
Jeppe Steen <61780753+Jeppe-Steen@users.noreply.github.com>
2026-03-16 11:43:28 +0100

    change footer

--- components/Footer.vue
+++ components/Footer.vue
@@ -1,7 +1,7 @@
 <template>
     <footer class="footer-component">
       <section class="footer-component__section">
-        <article>
+        <article class="links">
           <h3>Links</h3>
           <ul>
             <li><NuxtLink to="/">Forside</NuxtLink></li>
@@ -13,8 +13,9 @@
             <li><NuxtLink to="/#faq">Ofte stillede spørgsmål</NuxtLink></li>
           </ul>
         </article>
-        <address>
-          <h3>Oplysninger</h3>
+
+        <address class="contact">
+          <h3>Kontakt</h3>
           <p>Sandmosevej 11, 9440 Aabybro</p>
           <p>Tlf: 22 31 39 69</p>
           <p>Mail: info@klinikegernboakupunktur.dk</p>
@@ -26,7 +27,8 @@
           <p>Fredag: 8:00 - 12:30</p>
           <p>Lørdag - Søndag: Lukket</p>
         </address>
-        <article>
+
+        <article class="map">
           <iframe title="google_maps" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2163.6064533883787!2d9.696345913228498!3d57.160883503077045!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x464925c8a65bab49%3A0xb1b741175a25d19e!2sSandmosevej%2011%2C%209440%20Aabybro!5e0!3m2!1sda!2sdk!4v1768941476115!5m2!1sda!2sdk" width="600" height="450" style="border:0;" allowfullscreen="false" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
         </article>
     </section>
@@ -53,43 +55,52 @@ $darkGreen:#778873;
   };
 
   &__section {
-    display: flex;
-    flex-direction: column;
-    justify-content: space-between;
+    display: grid;
+    grid-template-areas: 
+      "links"
+      "contact"
+      "."
+      "map";
+    grid-template-columns: 1fr;
+    grid-template-rows: 1fr 1fr 0.5fr auto;
 
      // large screens
     @media (min-width: 1200px) {
-          flex-direction: row;
+      grid-template-areas: "links contact . map";
+      grid-template-columns: 1fr 1fr 0.5fr 1.5fr;
+      grid-template-rows: 1fr;
     };
+    
+    .links {
+      grid-area: links;
 
-     article {
-      margin-bottom: 50px;
-
-      @media (min-width: 1200px) {
-        margin: unset;
-        width: 30%;
+      li {
+        list-style: circle;
+        &:hover {
+          list-style: unset;
+        }
+        a {
+          color: black;
+          text-decoration: none;
+        }
       }
     }
 
-  }
-
-  li {
-    list-style: circle;
-    &:hover {
-      list-style: unset;
+    .contact {
+      grid-area: contact;
     }
-    a {
-      color: black;
-      text-decoration: none;
-    }
-  }
 
-  iframe {
-    height: unset;
-    width: 100%;
+    .map {
+      grid-area: map;
 
-    @media (min-width: 1200px) {
-      height: 300px;
+      iframe {
+        height: unset;
+        width: 100%;
+
+        @media (min-width: 1200px) {
+          height: 300px;
+        }
+      }
     }
   }
 }
