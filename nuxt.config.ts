de197464a7e16fbefa1631c34f942e91c9ffc260
Jeppe Steen <61780753+Jeppe-Steen@users.noreply.github.com>
2026-03-17 22:29:42 +0100

    robots and sitemap

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,7 +2,10 @@
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
-  modules: ['@nuxtjs/google-fonts'],
+  modules: [
+    '@nuxtjs/google-fonts',
+    '@nuxtjs/robots',
+    '@nuxtjs/sitemap'],
   googleFonts: {
     families: {
       Roboto: true, // Basic usage
