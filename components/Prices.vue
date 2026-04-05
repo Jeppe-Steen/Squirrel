23cb53e9dbbe08a9d37f98153ee630e4d59000fa
Jeppe Steen <61780753+Jeppe-Steen@users.noreply.github.com>
2026-04-05 14:28:35 +0200

    test

--- components/Prices.vue
+++ components/Prices.vue
@@ -29,6 +29,17 @@ onMounted(() => {
             <h1>Priser for behandlinger</h1>
         </header>
 
+        <div class="text-section">
+            <p>Er du omfattet af en sundhedsforsikring, har du mulighed for at få dækket dine akupunkturbehandlinger. Kontakt dit forsikringsselskab inden du påbegynder behandlingen.</p>
+            <br/>
+            <p>”Danmark” giver tilskud til akupunkturbehandling, ring og hør nærmere.</p>
+            <br/>
+            <p>Vedrørende RAB registrering <a href="https://stps.dk/sundhedsfaglig/registrering/registreringsordning-for-alternative-behandlere-rab" target="_blank">se her</a>. (link til styrelsen for patientsikkerhed)</p>
+        </div>
+
+
+
+
         <h2 class="prices-component__subheading">Behandlinger</h2>
         <div class="price-container">
             <PriceShell v-for="price in allPrices.filter(price => price.type === 'general')" :key="price.id" v-bind="price" />
@@ -37,11 +48,12 @@ onMounted(() => {
         <h2 class="prices-component__subheading">Klippekort</h2>
         <div class="price-container">
             <PriceShell v-for="price in allPrices.filter(price => price.type === 'klippekort').reverse()" :key="price.id" v-bind="price" />
+            <p>*Kan ikke købes i forbindelse med et forløb igennem sundhedsforsikring</p>
         </div>
 
         <h2 class="prices-component__subheading">Cupping</h2>
         <div class="price-container">
-            <PriceShell v-for="price in allPrices.filter(price => price.type === 'cupping')" :key="price.id" v-bind="price" />
+            <PriceShell v-for="price in allPrices.filter(price => price.type === 'cupping').reverse()" :key="price.id" v-bind="price" />
         </div>
 
         <h2 class="prices-component__subheading">Rygestop</h2>
@@ -62,16 +74,16 @@ $darkGreen:#778873;
     width: 100%;
     height: fit-content;
 
-    &__header {
-        grid-area: header;
-    }
-
     &__subheading {
         width: 100%;
-        text-align: center;
-        background-color: $lightestGreen;
+        text-align: left;
         padding: 10px;
         margin-top: 10px;
+    }
+
+    .text-section {
+        width: 100%;
+        margin: 20px 0;
     }
 
     .price-container {
